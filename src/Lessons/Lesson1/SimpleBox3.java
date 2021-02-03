9a5bf37c50a63fcca37089af0d749487f5469666
shumvlesu <sashumihin@gmail.com>
2021-02-03 18:12:01 +0300

    1 урок 49:47

--- src/Lessons/Lesson1/SimpleBox3.java
+++ src/Lessons/Lesson1/SimpleBox3.java
@@ -3,7 +3,7 @@ package Lessons.Lesson1;
 //класс обертка , он же дженерик
 public class SimpleBox3<T extends Number> {//T extends Number - так мы ограничиваем общем классом Number
   // и говорим что тип может быть только из числовых типов (Double, Long, Integer и т.д.)
-
+  //еще один ньюанс мы наследуем тип от класса Number. Класс SimpleBox3 от Number ничего не наследует
   private T obj;
 
   public SimpleBox3(T obj) {
