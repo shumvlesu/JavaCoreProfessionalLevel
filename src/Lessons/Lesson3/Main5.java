4d08ce37628c89c7c6a8b493f8e44d2b0c7a6242
sergey <sashumihin@gmail.com>
2021-02-14 19:14:31 +0300

    3 урок 1:17:41

--- src/Lessons/Lesson3/Main5.java
+++ src/Lessons/Lesson3/Main5.java
@@ -1,7 +1,25 @@
 package Lessons.Lesson3;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class Main5 {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws IOException {
+    byte[] array = new byte[10];
+    Arrays.fill(array, (byte) 66);
+    //второй параметр это размер буфера
+    BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("src/Lessons/Lesson3/text2.txt",true),5);
+    for (int i = 0; i < array.length; i++) {
+      //когда буфер заполниться (у нас он размера 5), тогда в файл запишутся элементы массива
+      //размер буфера(5) меньше размера массива (10), мы потеряли половину данных
+      bufferedOutputStream.write(array[i]);
+    }
+    //flush() помогает протолкгуть остальные данные нашего массива,на которые у нас не хватило буфера, в FileOutputStream
+    bufferedOutputStream.flush();
+    //при flush надо закрывать поток
+    bufferedOutputStream.close();
+
+    //BufferedInputStream работает по аналогии только в обратную сторону
+    BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("src/Lessons/Lesson3/text2.txt"));
   }
 }
