a0fed2f92c3474d7819dfaf307873bdc3cc6089b
sergey <sashumihin@gmail.com>
2021-02-14 21:12:55 +0300

    3 урок 1:50:27

---
+++ src/Lessons/Lesson3/Main8.java
@@ -0,0 +1,18 @@
+package Lessons.Lesson3;
+
+import java.io.*;
+
+public class Main8 {
+    public static void main(String[] args) throws IOException {
+        //символьные потоки
+        BufferedWriter writer = new BufferedWriter(new FileWriter("src/Lessons/Lesson3/text2.txt", true));
+        writer.write("llllll");
+        writer.flush();//это буферизированный метод, надо сделать flush()
+        BufferedReader reader = new BufferedReader(new FileReader("src/Lessons/Lesson3/text2.txt"));
+        
+        while (reader.ready()){//можем ли еще что то считать
+            System.out.println(reader.readLine());
+        }
+
+    }
+}
