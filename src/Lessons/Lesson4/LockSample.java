cca05190722f85e51d8cabf2ac511fff9f697afa
shumvlesu <sashumihin@gmail.com>
2021-02-19 16:43:51 +0300

    4 урок 1:14:17

--- src/Lessons/Lesson4/LockSample.java
+++ src/Lessons/Lesson4/LockSample.java
@@ -1,5 +1,38 @@
 package Lessons.Lesson4;
 
+//Пример монитора (мьютакса)
+//Пример синхронизации через методы
 public class LockSample {
-  
+  public static void main(String[] args) {
+    LockSample monitor = new LockSample();
+    new Thread(() -> monitor.method1()).start(); //объект monitor заблокирован потоком method1. Пока он не выполниться method2 не начнет работу.
+    // method2 видит что method1 synchronized (синхранизированный) и будет ждать пока method1 не выполниться. это работает при условии что method2
+    //тоже synchronized
+    new Thread(() -> monitor.method2()).start();
+    //будет так
+    //Thread-0 : Привет!
+    //Thread-0 : Привет!
+    //Thread-0 : Привет!
+    //Thread-1 : Пока!
+    //Thread-1 : Пока!
+    //Thread-1 : Пока!
+  }
+
+  //synchronized можно применить как на уровне метода так и на уровне объекта
+  public synchronized void method1() {
+    for (int i = 0; i < 3; i++) {
+      printMessage("Привет!");
+    }
+  }
+
+  public synchronized void method2() {
+    for (int i = 0; i < 3; i++) {
+      printMessage("Пока!");
+    }
+  }
+
+  public void printMessage(String message) {
+    System.out.println(Thread.currentThread().getName() + " : " + message);
+  }
+
 }
---
+++ src/Lessons/Lesson4/LockSample2.java
@@ -0,0 +1,50 @@
+package Lessons.Lesson4;
+
+//Пример монитора (мьютакса)
+//Пример синхронизации через объект
+public class LockSample2 {
+
+  Object lock = new Object();
+
+  public static void main(String[] args) {
+    LockSample2 lockSample2 = new LockSample2();
+    new Thread(() -> lockSample2.method1()).start();
+    new Thread(() -> lockSample2.method1()).start();
+//    new Thread(() -> lockSample2.method2()).start();
+
+  }
+
+  public void method1() {
+
+    printMessage("Начало не сихрнизированного блока");
+    try {
+      printMessage("ИБД");
+      Thread.sleep(3000);//спим 3 секунды
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    printMessage("Начало не сихрнизированного блока");
+
+    //синхронизация по объекту
+    //когда объект lockSample2 войдет в метод method1 он не навесит блокировку на lockSample2
+    //он заблокирует объект lock
+    synchronized (lock) {
+      printMessage("Начало сихрнизированного блока");
+      for (int i = 0; i < 3; i++) {
+        printMessage("Привет!");
+      }
+      printMessage("Конец сихрнизированного блока");
+    }
+  }
+
+//  public void method2() {
+//    for (int i = 0; i < 3; i++) {
+//      printMessage("Пока!");
+//    }
+//  }
+
+  public void printMessage(String message) {
+    System.out.println(Thread.currentThread().getName() + " : " + message);
+  }
+
+}
