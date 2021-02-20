c9a484546be75a02fad5ff4e2a8dce92f7b21891
shumvlesu <sashumihin@gmail.com>
2021-02-20 16:02:39 +0300

    4 урок 2:08:03

--- src/Lessons/Lesson4/ExecutorSample.java
+++ src/Lessons/Lesson4/ExecutorSample.java
@@ -1,4 +1,33 @@
 package Lessons.Lesson4;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//Пример ExecutorService
 public class ExecutorSample {
+  public static void main(String[] args) {
+    ExecutorService executorService = Executors.newFixedThreadPool(3);//Создает пул потоков с числом которое мы сам указываем
+    executorService = Executors.newCachedThreadPool();//Создает пул потоков без верхнего предела, т.е. динамически раширяемый
+    //опасен тем что оставляет в себе все потоки и может просто засрать память
+
+    executorService = Executors.newSingleThreadExecutor();//Создается пул потоков из одного потока
+
+    Runnable runnable = new Runnable() {
+      @Override
+      public void run() {
+        System.out.println(Thread.currentThread().getName());
+        try {
+          Thread.sleep(3000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    };
+    //передаю в пулл объект класса Runnable
+    executorService.execute(runnable);
+    //при newSingleThreadExecutor() (пуле одном )
+    executorService.execute(runnable);
+    executorService.execute(runnable);
+
+  }
 }
