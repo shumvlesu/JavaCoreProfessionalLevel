a66d8bfbe6821424d075ff26b4e287004ec5815e
shumvlesu <sashumihin@gmail.com>
2021-02-25 14:20:35 +0300

    5 урок ДЗ, остался семафор для тоннеля

--- src/HomeWorks/HomeWork5/Car.java
+++ src/HomeWorks/HomeWork5/Car.java
@@ -1,5 +1,7 @@
 package HomeWorks.HomeWork5;
 
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
   private static int CARS_COUNT;
 
@@ -10,6 +12,8 @@ public class Car implements Runnable {
   private Race race;
   private int speed;
   private String name;
+  private CyclicBarrier cyclicBarrier;
+
 
   public String getName() {
     return name;
@@ -19,24 +23,34 @@ public class Car implements Runnable {
     return speed;
   }
 
-  public Car(Race race, int speed) {
+  public Car(Race race, int speed, CyclicBarrier cyclicBarrier) {
     this.race = race;
     this.speed = speed;
     CARS_COUNT++;
     this.name = "Участник #" + CARS_COUNT;
+    this.cyclicBarrier = cyclicBarrier;
   }
 
   @Override
   public void run() {
     try {
       System.out.println(this.name + " готовится");
+      cyclicBarrier.await();
       Thread.sleep(500 + (int) (Math.random() * 800));
       System.out.println(this.name + " готов");
+      cyclicBarrier.await();
     } catch (Exception e) {
       e.printStackTrace();
     }
+    MainClass.countDownLatch.countDown();
     for (int i = 0; i < race.getStages().size(); i++) {
       race.getStages().get(i).go(this);
     }
+    //Определяем победителя флажком
+    if (!MainClass.winner){
+      MainClass.winner = true;
+      System.out.println(name + " " + "- WIN!");
+    }
+    MainClass.countDownLatch.countDown();
   }
 }
--- src/HomeWorks/HomeWork5/MainClass.java
+++ src/HomeWorks/HomeWork5/MainClass.java
@@ -1,19 +1,40 @@
 package HomeWorks.HomeWork5;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class MainClass {
   public static final int CARS_COUNT = 4;
+  public static CountDownLatch countDownLatch;
+  public static volatile boolean winner = false;
 
-  public static void main(String[] args) {
+  public static void main(String[] args)  {
     System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
     Race race = new Race(new Road(60), new Tunnel(), new Road(40));
     Car[] cars = new Car[CARS_COUNT];
+
+    CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
+    countDownLatch = new CountDownLatch(CARS_COUNT);
     for (int i = 0; i < cars.length; i++) {
-      cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+      cars[i] = new Car(race, 20 + (int) (Math.random() * 10),cyclicBarrier);
     }
+
     for (int i = 0; i < cars.length; i++) {
       new Thread(cars[i]).start();
     }
-    System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+    try {
+      //Дожидаемся пока все машины не будут готовы к гонке
+      countDownLatch.await();
+      System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+      //++Дожидаемся пока все машины не достигнут финиша
+      countDownLatch = new CountDownLatch(CARS_COUNT);
+      countDownLatch.await();
+      //--
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
   }
 }
