9c17e75e9c678857d898e5884350d54777858623
shumvlesu <sashumihin@gmail.com>
2021-02-26 15:48:02 +0300

    6 урок начало

---
+++ src/Lessons/Lesson6/Main.java
@@ -0,0 +1,10 @@
+package Lessons.Lesson6;
+
+
+
+public class Main {
+ // private static final Logger LOG = LogManager.getLogManager().getLogger(Main.class);
+  public static void main(String[] args) {
+
+  }
+}
