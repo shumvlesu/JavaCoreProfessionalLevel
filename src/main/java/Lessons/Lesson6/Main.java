48bfcbfd24128b27ab07a5d09c571751c954168f
shumvlesu <sashumihin@gmail.com>
2021-02-26 16:00:18 +0300

    6 урок. Добавил maven.

---
+++ src/main/java/Lessons/Lesson6/Main.java
@@ -0,0 +1,19 @@
+package Lessons.Lesson6;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+//что бы добавить maven в обычный проект
+//Нажать на папку с проектом правой кнопкой
+//Выбрать "Добавить поддержку фреймворка" введите сюда описание изображения
+//Выбрать из списка Maven
+
+//Затем добавляем библиотеку(-и) в свой проект - File -- Project structure -- Project settings -- Libraries, нажимаем + и выбираем библиотеки из файловой системы.
+//искал log4j
+
+public class Main {
+  private static final Logger LOG = LogManager.getLogger(Main.class);
+  public static void main(String[] args) {
+
+  }
+}
