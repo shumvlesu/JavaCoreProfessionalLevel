654696a6006593e71ec7b22843ce89d3691ec4c1
shumvlesu <sashumihin@gmail.com>
2021-03-01 09:54:37 +0300

    6 урок

--- src/test/java/Lessons/Lesson6/CalculatorTest.java
+++ src/test/java/Lessons/Lesson6/CalculatorTest.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
